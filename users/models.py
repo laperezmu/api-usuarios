f24df73fafe14d2b4e8cee2ffa5e7ad77f3d6228
Luis Alberto Pérez <laperezmu@gmail.com>
2021-10-08 21:16:59 -0500

    makemigrate y migrate del modelo de la tabla users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+class users(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidoP = models.CharField(max_length=50)
+    apellidoM = models.CharField(max_length=50)
+    edad = models.PositiveIntegerField()
+    email = models.EmailField(max_length=60)
+    telefono = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nombre
